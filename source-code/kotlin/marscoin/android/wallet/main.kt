b6d27cf05005bfeeadd44199013e289dd513faf8
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2026-03-20 16:52:36 +0100

    Created main.kt

---
+++ source-code/kotlin/marscoin/android/wallet/main.kt
@@ -0,0 +1,14 @@
+package marscoin.android.wallet;
+
+import android.os.Build;
+import android.os.Bundle;
+
+class main : Activity () {
+
+    override fun onCreate(savedInstance : Bundle) {
+
+        super.onCreate(savedInstanceState);
+        
+    };
+    
+};
