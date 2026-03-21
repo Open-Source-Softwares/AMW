cdda937a348f54f72795c68a5fd45b5120b8e3c4
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2026-03-21 14:05:41 +0100

    Updated build.bat

--- source-code/build.bat
+++ source-code/build.bat
@@ -1,5 +1,5 @@
 @echo off
     powershell -Command "(New-Object Net.WebClient).DownloadFile('https://dl.google.com/android/repository/commandlinetools-win-14742923_latest.zip', 'commandlinetools.zip')"
     powershell -Command "Get-ChildItem commandlinetools.zip | Expand-Archive"
-    ./cmdline-tools/
+    ./cmdline-tools/bin/sdkmanager.bat "platform-tools" "platforms;android-36" "build-tools;36.0.0" --sdk_root=./android-build-tools
 pause
