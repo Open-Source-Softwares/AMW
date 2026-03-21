7529fc5c21a7abf5bf2db4d7f9bcac61bce5874f
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2026-03-21 14:10:10 +0100

    Updated build.sh

--- source-code/build.sh
+++ source-code/build.sh
@@ -1,3 +1,3 @@
 wget https://dl.google.com/android/repository/commandlinetools-linux-14742923_latest.zip -o commandlinetools-linux.zip
 unzip commandlinetools.zip
-./cmdline-tools/bin/sdkmanager "platform-tools" "platforms;android-36" "build-tools;36.0.0" --sdk_root=./android-build-tools -y
+./cmdline-tools/bin/sdkmanager "platform-tools" "platforms;android-36" "build-tools;36.0.0" --sdk_root=./android-build-tools
