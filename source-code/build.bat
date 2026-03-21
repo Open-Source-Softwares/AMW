e3e40837f970ed85d7186111ddd8de374c9d6048
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2026-03-21 13:56:16 +0100

    Updated build.bat

--- source-code/build.bat
+++ source-code/build.bat
@@ -1,3 +1,5 @@
 @echo off
-
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://dl.google.com/android/repository/commandlinetools-win-14742923_latest.zip', 'commandlinetools.zip')"
+    powershell -Command "Get-ChildItem commandlinetools.zip | Expand-Archive"
+    ./cmdline-tools/
 pause
